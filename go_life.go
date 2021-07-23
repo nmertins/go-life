26ff974b8af3af0ea17b1b12ac128b864400fc1d
Nathan Mertins <nwmertins@gmail.com>
2021-07-23 07:14:44 -0500

    extract function for finding neighbors

--- go_life.go
+++ go_life.go
@@ -7,47 +7,9 @@ func Update(world [][]bool) [][]bool {
 		updated[i] = make([]bool, len(world[i]))
 	}
 
-	rows := len(world)
-	columns := len(world[0])
 	for i, row := range world {
 		for j := range row {
-			top_edge := i == 0
-			bottom_edge := i == rows-1
-			left_edge := j == 0
-			right_edge := j == columns-1
-
-			var neighbors []bool
-			if !top_edge {
-				neighbors = append(neighbors, world[i-1][j])
-			}
-
-			if !bottom_edge {
-				neighbors = append(neighbors, world[i+1][j])
-			}
-
-			if !left_edge {
-				neighbors = append(neighbors, world[i][j-1])
-			}
-
-			if !right_edge {
-				neighbors = append(neighbors, world[i][j+1])
-			}
-
-			if !(top_edge || left_edge) {
-				neighbors = append(neighbors, world[i-1][j-1])
-			}
-
-			if !(top_edge || right_edge) {
-				neighbors = append(neighbors, world[i-1][j+1])
-			}
-
-			if !(bottom_edge || left_edge) {
-				neighbors = append(neighbors, world[i+1][j-1])
-			}
-
-			if !(bottom_edge || right_edge) {
-				neighbors = append(neighbors, world[i+1][j+1])
-			}
+			neighbors := getNeighbors(i, j, world)
 
 			live_neighbors := 0
 			living := world[i][j]
@@ -68,4 +30,49 @@ func Update(world [][]bool) [][]bool {
 	}
 
 	return updated
+}
+
+func getNeighbors(i, j int, world [][]bool) []bool {
+	rows := len(world)
+	columns := len(world[0])
+
+	top_edge := i == 0
+	bottom_edge := i == rows-1
+	left_edge := j == 0
+	right_edge := j == columns-1
+
+	var neighbors []bool
+	if !top_edge {
+		neighbors = append(neighbors, world[i-1][j])
+	}
+
+	if !bottom_edge {
+		neighbors = append(neighbors, world[i+1][j])
+	}
+
+	if !left_edge {
+		neighbors = append(neighbors, world[i][j-1])
+	}
+
+	if !right_edge {
+		neighbors = append(neighbors, world[i][j+1])
+	}
+
+	if !(top_edge || left_edge) {
+		neighbors = append(neighbors, world[i-1][j-1])
+	}
+
+	if !(top_edge || right_edge) {
+		neighbors = append(neighbors, world[i-1][j+1])
+	}
+
+	if !(bottom_edge || left_edge) {
+		neighbors = append(neighbors, world[i+1][j-1])
+	}
+
+	if !(bottom_edge || right_edge) {
+		neighbors = append(neighbors, world[i+1][j+1])
+	}
+
+	return neighbors
 }
